a26c3abc67704c11fa2ef01109d740f44c0f94bb
Andres Ulloa Schmeisser <andresjk99@gmail.com>
2024-06-18 18:44:05 -0400

    Update connect-openvpn-client.sh

--- connect-openvpn-client.sh
+++ connect-openvpn-client.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-echo "Enter .ovpn profile full path, e.g /home/myusername/vpn-profile.ovpn:"
-read PROFILE
+PROFILE=`pwd`/profile.ovpn
 openvpn3 config-import --config $PROFILE --name VSaaSVPN --persistent
 openvpn3 config-acl --show --lock-down true --grant root --config VSaaSVPN
 sudo systemctl enable --now openvpn3-session@VSaaSVPN.service
