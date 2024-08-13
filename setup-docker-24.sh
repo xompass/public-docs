66b7bd549424c301340eeb0253cab29ac38d4c43
Andres Ulloa Schmeisser <andresjk99@gmail.com>
2024-08-13 18:14:07 -0400

    Update setup-docker-24.sh

--- setup-docker-24.sh
+++ setup-docker-24.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt update && apt upgrade
+sudo apt update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
