6f990f9340f75a7b1eb972a24b354316ba880935
Andres Ulloa Schmeisser <andresjk99@gmail.com>
2024-12-11 15:07:42 -0300

    Update connect-openvpn-client.sh

--- connect-openvpn-client.sh
+++ connect-openvpn-client.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 PROFILE=`pwd`/profile.ovpn
-openvpn3 config-import --config $PROFILE --name VSaaSVPN --persistent
-openvpn3 config-acl --show --lock-down true --grant root --config VSaaSVPN
-sudo systemctl enable --now openvpn3-session@VSaaSVPN.service
+
+if uname -m | grep -q 'aarch64'; then
+    sudo cp $PROFILE /etc/openvpn/client.conf
+    sudo systemctl enable openvpn@client
+    sudo systemctl start openvpn@client
+fi
+
+if uname -m | grep -q 'x86_64'; then
+    openvpn3 config-import --config $PROFILE --name VSaaSVPN --persistent
+    openvpn3 config-acl --show --lock-down true --grant root --config VSaaSVPN
+    sudo systemctl enable --now openvpn3-session@VSaaSVPN.service
+fi
