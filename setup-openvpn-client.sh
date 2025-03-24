4cef430689c8d6bfb17b09b4c0584cc57a0d0844
Andres Ulloa Schmeisser <andresjk99@gmail.com>
2025-03-24 10:07:15 -0300

    Update setup-openvpn-client.sh

--- setup-openvpn-client.sh
+++ setup-openvpn-client.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 sudo apt update
 
-if uname -m | grep -q 'aarch64'; then
-  sudo apt install -y openvpn
-fi
-
-if uname -m | grep -q 'x86_64'; then
-  sudo mkdir -p /etc/apt/keyrings && curl -fsSL https://packages.openvpn.net/packages-repo.gpg | sudo tee /etc/apt/keyrings/openvpn.asc
-  DISTRO=$(lsb_release -c | awk '{print $2}')
-  echo "deb [signed-by=/etc/apt/keyrings/openvpn.asc] https://packages.openvpn.net/openvpn3/debian $DISTRO main" | sudo tee /etc/apt/sources.list.d/openvpn-packages.list
-  sudo apt install -y openvpn3
-fi
+sudo apt install -y openvpn
 
